0bc93043f101f7cb8b47a3a38ec3b050f4d040f1
Chris Song <chris.bk.song@gmail.com>
2020-04-02 10:08:00 -0400

    fixed again

--- count_odds.py
+++ count_odds.py
@@ -7,10 +7,15 @@ result = 0
 # Work!
 # Let's look at the numbers from 0 to 5000
 num = 0
-while num <= 5000:
-    print(num)
+while num <= 20:
+    is_odd = num % 2 != 0
+    if is_odd:
+        print(num)
+        result += num
+    # print('I am inside the loop')
+    # print(num)
     num = num + 1 #(longer version)
-    # num += 1 (shorter version/ choose whichever makes more sense)
+    # num += 1 #(shorter version/ choose whichever makes more sense)
 
 
 
