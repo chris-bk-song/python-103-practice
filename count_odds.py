8102edc37156fbdfeb7e51463c1b62c8351e6a78
Chris Song <chris.bk.song@gmail.com>
2020-04-02 09:51:46 -0400

    Program for counting odd numbers

---
+++ count_odds.py
@@ -0,0 +1,18 @@
+# Add the odd numbers between 0 to 5000 (purpose of my program)
+
+# Setup
+result = 0
+
+
+# Work!
+# Let's look at the numbers from 0 to 5000
+num = 0
+while num <= 5000:
+    print(num)
+    num = num + 1 #(longer version)
+    # num += 1 (shorter version/ choose whichever makes more sense)
+
+
+
+# Return/print the result
+print(result)
